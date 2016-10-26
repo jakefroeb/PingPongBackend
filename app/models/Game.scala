4502d2fa7704fcfa088ecb7366517f861e179bcc
Jake Froeb <jakefroeb@Jakes-MacBook-Pro.local>
2016-10-26 15:36:28 -0500

    and yet another

--- app/models/Game.scala
+++ app/models/Game.scala
@@ -11,15 +11,15 @@ import play.api.db.slick.Config.driver.simple._
 
 case class Players(player_1 :String, score_1: String, player_2 : String, score_2:String)
 
-case class Game(player_1 :String, player_2:String,  score_1: Int,  score_2:Int, date: String, id: Option[Int] = None)
+case class Game(player_1 :String, player_2:String,  score_1: String,  score_2:String, date: String, id: Option[String] = None)
 
 class GamesTable(tag: Tag) extends Table[Game](tag, "games") {
   def player1 = column[String]("player_1", O.NotNull)
   def player2 = column[String]("player_2", O.NotNull)
-  def score1 = column[Int]("score_1", O.NotNull)
-  def score2 = column[Int]("score_2", O.NotNull)
+  def score1 = column[String]("score_1", O.NotNull)
+  def score2 = column[String]("score_2", O.NotNull)
   def date = column[String]("date", O.NotNull)
-  def id = column[Int]("id",O.AutoInc,O.PrimaryKey)
+  def id = column[String]("id",O.AutoInc,O.PrimaryKey)
 
 
   def * = (player1,player2,score1,score2,date,id.?) <> (Game.tupled, Game.unapply _)
